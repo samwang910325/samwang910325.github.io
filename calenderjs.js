6c809dca1e0b468a2c4a6c455d35d37ca312c36b
unknown <samwang910325@gmail.com>
2021-09-13 14:49:11 +0800

    on

---
+++ calenderjs.js
@@ -0,0 +1,37 @@
+
+document.getElementById("bt").onclick = function() {myFunction()};
+
+function myFunction() {
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function(){
+		var input;
+		input = this.responseText;
+		PutInTable(input);
+	}
+	xhttp.open("GET", "calender.txt");
+	xhttp.send();
+}
+
+function PutInTable(input){ 
+	const arr = ["","","","","","","",""];
+	var index;
+	for(var i = 0 ; i < input.length ; i++){
+		if(input[i] == '/'){
+			arr[index] += "<br>"
+			index = input[i+1];
+			i = i + 1;
+		}
+		arr[index] += input[i];
+		
+	}
+	var trows = document.getElementById("table").rows;
+	var tcols = trows[1].cells; 
+
+	for(var i = 1 ; i < 7 ; i++){
+		tcols[i - 1].innerHTML = arr[i];  
+	}
+	
+	
+	
+	
+}
